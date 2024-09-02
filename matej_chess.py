2b21f39631a05e9697d397582d0a5422e064eff2
matejpopovski <matej.popovski@gmail.com>
2024-09-01 19:19:29 -0500

    testing leetcode api

--- matej_chess.py
+++ matej_chess.py
@@ -1,7 +1,8 @@
 #Header: Matej Popovski
 import requests
 
-data = requests.get("")
-weather_data = data.json()
-
-#changes
+data = requests.get("https://leetcode-stats-api.herokuapp.com/matejpopovski")
+print(data)
+print('-----------')
+data = data.json()
+print(data)
